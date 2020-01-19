582978bdef503ea6b207d65424d67891acffcc2b
jsullivanp96 <34928654+jsullivanp96@users.noreply.github.com>
2020-01-19 10:53:53 -0800

    Create data.py
    data of websites

---
+++ data.py
@@ -0,0 +1,272 @@
+
+a = dict()
+
+a_100 = dict()
+a_100["apn"] = '1000'
+a_100["roomNum"] = 409
+a_100['bedrooms'] = 2
+a_100['bathrooms'] = 1
+a_100["sqrFt"] = 630
+a_100['rent'] = 1940
+
+a_101 = dict()
+a_101["apn"] = '2000'
+a_101["roomNum"] = 312
+a_101['bedrooms'] = 2
+a_101['bathrooms'] = 2
+a_101["sqrFt"] = 714
+a_101['rent'] = 2150
+
+a['address'] = '363 Western Dr, Santa Cruz, CA 95060'
+a['description'] = "The Hilltop Santa Cruz offers 1 and 2 bedroom apartments in the center of all things that make Santa Cruz unique and lovable. With tons of great amenities and a great selection of floor plans to choose from, you'll love calling us home. You can focus on your studies, enjoy one of our awesome resident events, or cheer on the Slugs while we take care of the details. "
+a['website'] = "http://www.liveatthehilltop.com"
+a['photos']= ['http://www.liveatthehilltop.com/-/media/Hilltop/Photos/Models/HiRes_Hilltop_2019-05_04.jpg?h=3000&mh=921&mw=1382&thn=0&w=4500&crop=1&hash=79A59239F888EC637B689E51CACA84228B36ACDD',
+              'http://www.liveatthehilltop.com/-/media/Hilltop/Photos/Models/HiRes_Hilltop_2019-05_44.jpg?h=3000&mh=921&mw=1382&thn=0&w=4500&crop=1&hash=E620C6D045CBB1B05EC2B6CF79DB952858DCE9BF']
+a['amenities'] = "onsite laundry\noutdoor pool\ncommunity lounge\ncovered parking"
+a['units'] = [a_100, a_101]
+
+b = dict()
+
+b_100 = dict()
+b_100["apn"] = '3000'
+b_100["roomNum"] = 201
+b_100['bedrooms'] = 1
+b_100['bathrooms'] = 1
+b_100["sqrFt"] = 452
+b_100['rent'] = 1290
+
+b_101 = dict()
+b_101["apn"] = '4000'
+b_101["roomNum"] = 220
+b_101['bedrooms'] = 2
+b_101['bathrooms'] = 1
+b_101["sqrFt"] = 756
+b_101['rent'] = 2435
+
+
+b['address'] = '555 Pacific Ave, Santa Cruz, CA 95060'
+b['description'] = "Five55 is a brand new beachside apartment community offering luxury living in the heart of Santa Cruz. We offer a collection of well-designed residences sure to please even those with the most discriminating tastes. Whether you want to host a dinner party at the outdoor kitchen and pizza ovens, catch a sunset from the rooftop deck or walk into downtown or along the beach, Five55 is the perfect location for you!"
+b['website'] = "https://www.five55santacruz.com"
+b['photos']= ['https://capi.myleasestar.com/v2/dimg/60029190/1771x1481/60029190.jpg',
+              'https://capi.myleasestar.com/v2/dimg/59331887/1771x1481/59331887.jpg',
+              'https://capi.myleasestar.com/v2/dimg/59331886/1771x1481/59331886.jpg']
+b['amenities'] = "community lounge\npet friendly\nin-unit laundry\nbuilding garage"
+b['units'] = [b_100, b_101]
+
+c = dict()
+
+c_100 = dict()
+c_100["apn"] = '3000'
+c_100["roomNum"] = 108
+c_100['bedrooms'] = 3
+c_100['bathrooms'] = 2
+c_100["sqrFt"] = 812
+c_100['rent'] = 2345
+
+c['address'] = "143 Chestnut St, Santa Cruz, CA 95060"
+c['description'] = "Experience the essence of Santa Cruz living at Chestnut Street apartments in Santa Cruz, CA."
+c['website'] = "https://www.essexapartmenthomes.com/en/apartments/chestnut-street"
+c['photos']= ['https://ssl.c.photoshelter.com/img-get2/I0000wjotOgePPSU/sec=wdfsdfoeflwefms1440ed20200122mU9QrJiLmCdkGW7/fill=1680x945/02-Chestnut-Street-Apartment-Homes-Pet-Friendly-Santa-Cruz.jpg/fill=713x476',
+              'https://ssl.c.photoshelter.com/img-get2/I0000WjbCTloo1xM/sec=wdfsdfoeflwefms1440ed20200122NtmnYzQF7GX3E7z/fill=1680x945/03-Chestnut-Street-Apartment-Homes-BBQ-Area-Santa-Cruz.jpg/fill=713x476',
+              'https://ssl.c.photoshelter.com/img-get2/I0000TD1jqpIMIyY/sec=wdfsdfoeflwefms1440ed20200122JOhacSuvmfPnA0D/fill=1680x945/04-Chestnut-Street-Apartment-Homes-BBQ-Area-Santa-Cruz.jpg/fill=713x476']
+c['amenities'] = "BBQ area\ndog park\npet friendly"
+c['units'] = [c_100, c_101]
+
+d = dict()
+
+d_100 = dict()
+d_100["apn"] = '2000'
+d_100["roomNum"] = 203
+d_100['bedrooms'] = 2
+d_100['bathrooms'] = 1
+d_100["sqrFt"] = 586
+d_100['rent'] = 1995
+
+d['address'] = "101 Felix St, Santa Cruz, CA 95060"
+d['description'] = "Cypress Point offers a superbly convenient location close to all the best the area has to offer! "
+d['website'] = "https://www.liveatcypresspoint.com/"
+d['photos']= ['https://cdngeneral.rentcafe.com/dmslivecafe/3/651241/CP Front View.jpg?quality=85&scale=both&',
+              'https://cdngeneral.rentcafe.com/dmslivecafe/3/651241/CP%20Fitness.jpg?quality=85&scale=both&',
+              'https://cdngeneral.rentcafe.com/dmslivecafe/3/651241/CP Lounge.jpg?quality=85&scale=both&',
+              'https://cdngeneral.rentcafe.com/dmslivecafe/3/651241/Cypress Point Pool.jpg?crop=(0,0,299.1340206185569,124)&cropxunits=300&cropyunits=124&mode=crop&scale=both&quality=85',
+              ]
+d['amenities'] = "pool\nindoor gym\nspacious floor plans"
+d['units'] = [d_100]
+
+e = dict()
+
+e_100 = dict()
+e_100["apn"] = '1000'
+e_100["roomNum"] = 315
+e_100['bedrooms'] = 1
+e_100['bathrooms'] = 1
+e_100["sqrFt"] = 320
+e_100['rent'] = 1495
+
+e['address'] = "1010 Pacific Ave, Santa Cruz, CA 95060"
+e['description'] = "Live your own unique California dream at 1010 Pacific Apartments in the heart of Santa Cruz, CA."
+e['website'] = "https://www.1010pacificapts.com/"
+e['photos']= ['https://g5-assets-cld-res.cloudinary.com/image/upload/q_auto,f_auto,fl_lossy/g5/g5-c-imgkhas5-woodmont-real-estate-services-client/g5-cl-1gz1ejbzw5-1010-pacific-apartments/uploads/floorplans2.jpg',
+              'https://g5-assets-cld-res.cloudinary.com/image/upload/q_auto,f_auto,fl_lossy/g5/g5-c-imgkhas5-woodmont-real-estate-services-client/g5-cl-1gz1ejbzw5-1010-pacific-apartments/uploads/amenities1.jpg',
+              'https://g5-assets-cld-res.cloudinary.com/image/upload/q_auto,f_auto,fl_lossy/g5/g5-c-imgkhas5-woodmont-real-estate-services-client/g5-cl-1gz1ejbzw5-1010-pacific-apartments/uploads/floorplans1.jpg',
+              ]
+e['amenities'] = "pet friendly\nindoor gym\nunderground parking"
+e['units'] = [e_100]
+
+f = dict()
+
+f_100 = dict()
+f_100["apn"] = '5000'
+f_100["roomNum"] = 132
+f_100['bedrooms'] = 4
+f_100['bathrooms'] = 2
+f_100["sqrFt"] = 760
+f_100['rent'] = 3195
+
+f['address'] = "222 Columbia St, Santa Cruz, CA 95060"
+f['description'] = "Discover a restful retreat at Oceanview Apartments, a peaceful beach-side Community located just moments from everything that Santa Cruz has to offer."
+f['website'] = "https://www.oceanview-apartments.com/"
+f['photos']= ['https://medialibrarycdn.entrata.com/media_library/3351/5dcdd0a1825fe4.75640726733.jpg',
+              'https://medialibrarycdn.entrata.com/media_library/3351/5dcdcc548e3057.07553610609.jpg',
+              'https://medialibrarycdn.entrata.com/media_library/3351/5dcdcf1eb46f41.02263171158.jpg',
+              'https://medialibrarycdn.entrata.com/media_library/3351/5dcdd1f0e02d01.11887166714.jpg',
+              'https://commoncdn.entrata.com/images/thumbNailer.php?src=/media_library/3351/5b6de742b309b6.02110222240.jpg&w=4096&h=2734',
+              'https://commoncdn.entrata.com/images/thumbNailer.php?src=/media_library/3351/5b6de7421316e4.79032012367.jpg&w=4096&h=2734']
+f['amenities'] = "pool\ngated community\nlaundry facilities"
+f['units'] = [f_100]
+
+g = dict()
+
+g_100 = dict()
+g_100["apn"] = '1000'
+g_100["roomNum"] = 210
+g_100['bedrooms'] = 3
+g_100['bathrooms'] = 1
+g_100["sqrFt"] = 816
+g_100['rent'] = 2695
+
+g['address'] = "1240 Shaffer Rd, Santa Cruz, CA 95060"
+g['description'] = "Put yourself in the view at Pacific Shores in beautiful Santa Cruz, CA, bring the California dream to you with garden-style elegance."
+g['website'] = "https://www.pacificshoresapts.com/"
+g['photos']= ['https://g5-assets-cld-res.cloudinary.com/image/upload/q_auto,f_auto,c_fill,g_center,h_665,w_998/v1560445034/g5/g5-c-imgkhas5-woodmont-real-estate-services-client/g5-cl-1gz1gq5sdy-pacific-shores/uploads/09_b1iuv9.jpg',
+              'https://g5-assets-cld-res.cloudinary.com/image/upload/q_auto,f_auto,c_fill,g_center,h_398,w_598/v1560445031/g5/g5-c-imgkhas5-woodmont-real-estate-services-client/g5-cl-1gz1gq5sdy-pacific-shores/uploads/01_udkg7t.jpg',
+              'https://g5-assets-cld-res.cloudinary.com/image/upload/q_auto,f_auto,c_fill,g_center,h_665,w_998/v1560445036/g5/g5-c-imgkhas5-woodmont-real-estate-services-client/g5-cl-1gz1gq5sdy-pacific-shores/uploads/11_yuzbar.jpg',
+              'https://g5-assets-cld-res.cloudinary.com/image/upload/q_auto,f_auto,c_fill,g_center,h_665,w_998/v1560445029/g5/g5-c-imgkhas5-woodmont-real-estate-services-client/g5-cl-1gz1gq5sdy-pacific-shores/uploads/20_gpwdhl.jpg',
+              'https://g5-assets-cld-res.cloudinary.com/image/upload/q_auto,f_auto,c_fill,g_center,h_665,w_998/v1560445029/g5/g5-c-imgkhas5-woodmont-real-estate-services-client/g5-cl-1gz1gq5sdy-pacific-shores/uploads/16_pu9znv.jpg',
+              'https://g5-assets-cld-res.cloudinary.com/image/upload/q_auto,f_auto,fl_lossy/g5/g5-c-imgkhas5-woodmont-real-estate-services-client/g5-cl-1gz1gq5sdy-pacific-shores/uploads/gallery2.jpg']
+g['amenities'] = "pool\nlarge floor plans\noutdoor lounge area"
+g['units'] = [g_100]
+
+h = dict()
+
+h_100 = dict()
+h_100["apn"] = '2000'
+h_100["roomNum"] = 315
+h_100['bedrooms'] = 1
+h_100['bathrooms'] = 1
+h_100["sqrFt"] = 244
+h_100['rent'] = 2100
+
+h['address'] = "304 Cliff St, Santa Cruz, CA 95060"
+h['description'] = "The Sands Apartments is a private, gated community nestled in a gorgeous beachfront setting."
+h['website'] = "https://www.sands-apts.com/"
+h['photos']= ['https://cdngeneral.rentcafe.com/dmslivecafe/3/735326/02.jpg?&quality=85&width=956&height=717&mode=crop',
+              'https://cdngeneral.rentcafe.com/dmslivecafe/3/735326/CliffSt_HiRes-411111111111.jpg?crop=(0,0,300,200)&cropxunits=300&cropyunits=200&quality=85&width=956&height=717&mode=crop',
+              'https://cdngeneral.rentcafe.com/dmslivecafe/3/735326/13.jpg?&quality=85&width=956&height=717&mode=crop',
+              'https://cdngeneral.rentcafe.com/dmslivecafe/3/735326/06.jpg?&quality=85&width=956&height=717&mode=crop']
+h['amenities'] = "reserved parking\ngated community\npet friendly"
+h['units'] = [h_100]
+
+i = dict()
+
+i_100 = dict()
+i_100["apn"] = '1000'
+i_100["roomNum"] = 145
+i_100['bedrooms'] = 3
+i_100['bathrooms'] = 1
+i_100["sqrFt"] = 788
+i_100['rent'] = 2850
+
+i['address'] = "630 Water St, Santa Cruz, CA 95060"
+i['description'] = "Make yourself at home at The Breakers where quality living and convenience await. "
+i['website'] = "https://www.thebreakerssantacruz.com/"
+i['photos']= ['https://cdngeneral.rentcafe.com/dmslivecafe/3/513022/P0522652_02_The%20Breakers%20Apts__630WaterStreet_1-01.jpg?crop=(0,0,300,200)&cropxunits=300&cropyunits=200&quality=85',
+              'https://cdngeneral.rentcafe.com/dmslivecafe/3/513022/P0522652_09_The%20Breakers%20Apts__630WaterStreet_1-07.jpg?crop=(0,0,300,200)&cropxunits=300&cropyunits=200&quality=85&quality=80&width=515&height=315&mode=crop',
+              'https://cdngeneral.rentcafe.com/dmslivecafe/3/513022/P0522652_15_The%20Breakers%20Apts__630WaterStreet_1-12.jpg?crop=(0,0,300,200)&cropxunits=300&cropyunits=200&quality=85&quality=80&width=515&height=315&mode=crop',
+              'https://cdngeneral.rentcafe.com/dmslivecafe/3/513022/P0522652_12_The%20Breakers%20Apts_630WaterStreet_1-09.jpg?crop=(0,0,300,200)&cropxunits=300&cropyunits=200&quality=85&quality=80&width=515&height=315&mode=crop',
+              'https://cdngeneral.rentcafe.com/dmslivecafe/3/513022/P0522652_20_The%20Breakers%20Apts_for%20rent-16.jpg?crop=(0,0,300,200)&cropxunits=300&cropyunits=200&quality=85&quality=80&width=515&height=315&mode=crop']
+i['amenities'] = "reserved and covered parking\nlaundry facilities\npet friendly"
+i['units'] = [i_100]
+
+
+j = dict()
+
+j_100 = dict()
+j_100["apn"] = '4000'
+j_100["roomNum"] = 420
+j_100['bedrooms'] = 4
+j_100['bathrooms'] = 3
+j_100["sqrFt"] = 1100
+j_100['rent'] = 3449
+
+j['address'] = "200 Button St, Santa Cruz, CA 95060"
+j['description'] = "Tour today and see why you want to call Hidden Creek home!"
+j['website'] = "https://www.hiddencreekhomes.com/"
+j['photos']= ['https://cdngeneral.rentcafe.com/dmslivecafe/3/735325/hc-flowers_web.jpg?&quality=85&width=956&height=717&mode=crop',
+              'https://cdngeneral.rentcafe.com/dmslivecafe/3/735325/k2n46nwimt5r5yxuurxqcs3gzjgvubcvzyuu0oyq-(1)11111.jpg?&quality=85&width=956&height=717&mode=crop',
+              'https://cdngeneral.rentcafe.com/dmslivecafe/3/735325/q0rx57ia7k46e9rp2ea1uh9py216z0go2g7ir3jy111111.jpg?&quality=85&width=956&height=717&mode=crop',
+              'https://cdngeneral.rentcafe.com/dmslivecafe/3/735325/uqqh1rhed17y1vsir3kta3e5mbs54kqqt9seqvqy11111.jpg?&quality=85&width=956&height=717&mode=crop',
+              'https://cdngeneral.rentcafe.com/dmslivecafe/3/735325/0073creeksideapartmentssantacruzarchitecturephotography_web.jpg?&quality=85&width=956&height=717&mode=crop']
+j['amenities'] = "outdoor pool\noutdoor playground\npet friendly"
+j['units'] = [j_100]
+
+k = dict()
+
+k_100 = dict()
+k_100["apn"] = '1000'
+k_100["roomNum"] = 412
+k_100['bedrooms'] = 3
+k_100['bathrooms'] = 2
+k_100["sqrFt"] = 810
+k_100['rent'] = 2899
+
+k['address'] = "2500 Soquel Dr, Santa Cruz, CA 95065"
+k['description'] = "Inspired by our residents and our stunning Santa Cruz central coast setting, we have created an exceptional space of peace to provide a welcoming rental housing experience.  "
+k['website'] = "https://www.emeraldbayapartments.net/"
+k['photos']= ['https://mediaprocessor.websimages.com/fit/1920x1920/www.emeraldbayapartments.net/emerald-bay-apartments-santa-cruz-ca-building-photo.jpg',
+              'https://mediaprocessor.websimages.com/fit/1920x1920/www.emeraldbayapartments.net/ebayfront.jpg',
+              'https://mediaprocessor.websimages.com/fit/1920x1920/www.emeraldbayapartments.net/052.jpg',
+              'https://mediaprocessor.websimages.com/fit/1920x1920/www.emeraldbayapartments.net/counter.jpg']
+k['amenities'] = "gated community\noutdoor playground\ndedicated resident parking"
+k['units'] = [k_100]
+
+l = dict()
+
+l_100 = dict()
+l_100["apn"] = '3000'
+l_100["roomNum"] = 132
+l_100['bedrooms'] = 1
+l_100['bathrooms'] = 1
+l_100["sqrFt"] = 310
+l_100['rent'] = 999
+
+l['address'] = "120 San Lorenzo Blvd, Santa Cruz, CA 95060"
+l['description'] = "San Lorenzo Apartments are nestled in a gorgeous park like setting"
+l['website'] = "https://www.sanlorenzo-apts.com/"
+l['photos']= ['https://cdngeneral.rentcafe.com/dmslivecafe/3/735327/CA_SantaCruz_SanLorenzoCourtApartments_p0741076_19_6_1_PhotoGallery.jpg?&quality=85&width=956&height=717&mode=crop',
+              'https://cdngeneral.rentcafe.com/dmslivecafe/3/735327/SanLorenzo_HiRes-4_web.jpg?&quality=85&width=956&height=717&mode=crop',
+              'https://cdngeneral.rentcafe.com/dmslivecafe/3/735327/CA_SantaCruz_SanLorenzoCourtApartments_p0741076_14_21_1_PhotoGallery.jpg?&quality=85&width=956&height=717&mode=crop',
+              'https://cdngeneral.rentcafe.com/dmslivecafe/3/735327/CA_SantaCruz_SanLorenzoCourtApartments_p0741076_17_4_1_PhotoGallery.jpg?&quality=85&width=956&height=717&mode=crop',
+              'https://cdngeneral.rentcafe.com/dmslivecafe/3/735327/SanLorenzo_HiRes-7_web.jpg?&quality=85&width=956&height=717&mode=crop']
+l['amenities'] = "basketball courts\noutdoor playground\npet friendly"
+l['units'] = [l_100]
+
+m = dict()
+
+m_100 = dict()
+m_100["apn"] = '1000'
+m_100["roomNum"] = 231
+m_100['bedrooms'] = 2
+m_100['bathrooms'] = 2
+m_100["sqrFt"] = 750
+m_100['rent'] = 2500
